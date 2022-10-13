16197bb1c94848d3db5f14a6da4f3e810beeb02f
Eben <eben@glucode.com>
2022-10-13 08:30:36 +0200

    Create project

---
+++ BlackMap/BlackMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackMapApp.swift
+//  BlackMap
+//
+//  Created by Eben Kok on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct BlackMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
